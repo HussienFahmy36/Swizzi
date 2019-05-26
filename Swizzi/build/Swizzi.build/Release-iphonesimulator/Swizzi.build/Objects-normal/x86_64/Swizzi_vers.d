1bab3fc76044af6c5f9b726c70599f424645e2bc
hussein <hussein>
2019-05-26 13:28:58 +0200

    adding framework product with build shell script

---
+++ Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/Objects-normal/x86_64/Swizzi_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/hussiengamalmohammed/Documents/HussienWorkspace/Personal/Swizzi\ Github/Swizzi/Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/DerivedSources/Swizzi_vers.c
