41c528876181b877b50fd609bcb5c6cf17d83bb2
hussein <hussein>
2019-05-26 17:44:03 +0200

    add shell script to build universal framework

--- Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/Objects-normal/x86_64/Swizzi_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/hussiengamalmohammed/Documents/HussienWorkspace/Personal/Swizzi\ Github/Swizzi/Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/DerivedSources/Swizzi_vers.c
