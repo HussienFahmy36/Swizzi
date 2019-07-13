7bc9175c08de40d8057f425aad0ffbf160fb8991
hussein <hussein>
2019-07-13 18:22:53 +0200

    fix framework compilation error by referring to xcode project framewowk directly

--- Swizzi/build/Swizzi.build/Debug-iphonesimulator/Swizzi.build/Objects-normal/x86_64/Swizzi_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/hussiengamalmohammed/Documents/HussienWorkspace/Personal/Swizzi\ Github/Swizzi/Swizzi/build/Swizzi.build/Debug-iphonesimulator/Swizzi.build/DerivedSources/Swizzi_vers.c
---
+++ Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/Objects-normal/x86_64/Swizzi_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/hussiengamalmohammed/Desktop/Swizzi/Swizzi/build/Swizzi.build/Release-iphonesimulator/Swizzi.build/DerivedSources/Swizzi_vers.c
